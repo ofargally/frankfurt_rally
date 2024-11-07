0f9cd129d816e8e380e60dee8fde9d48841f1782
ofargally <ofargally@colgate.edu>
2024-11-07 09:06:18 +0100

    basic template

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,50 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-  return (
-    <>
+  const [passWord, setPassWord] = useState("0");
+  const [message, setMessage] = useState("");
+  const [redirection, setRedirection] = useState(false);
+  const correctAnswer = "1234";
+  const onSubmitHandle = () => {
+    if (passWord == correctAnswer) {
+      setMessage("Good Job, Now on to the next stage");
+      setRedirection(true);
+    } else {
+      setMessage("Wrong Try again!");
+    }
+  };
+  const showPasswordLogic = () => {
+    return (
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>
+          Congrats! You found the first link to the puzzle! Enter the secret
+          code to continue!
+        </h1>
+        <div className="card">
+          <input
+            type="string"
+            placeholder="Enter secret code"
+            value={passWord}
+            onChange={(e) => setPassWord(e.target.value)}
+          />
+        </div>
+        <button onClick={onSubmitHandle}>Submit</button>
+        {message && <p>{message}</p>}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+    );
+  };
+  const showPostPasswordLogic = () => {
+    return (
+      <div>
+        <p>The location is in xyz</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    );
+  };
+
+  return (
+    <div className="App">
+      {!redirection ? showPasswordLogic() : showPostPasswordLogic()}
+    </div>
   );
 }
 
